7a244994a9c66a9ec5e57d1134f6e01e80a3f284
Zac Lo <zlo120@live.com>
2023-05-20 16:53:22 +1000

    knex stuff

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const options = require("./knexfile.js");
+const knex = require("knex")(options);
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  req.db = knex;
+  next();
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.get("/knex", function (req, res, next) {
+  req.db
+    .raw("SELECT VERSION()")
+    .then((version) => console.log(version[0][0]))
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+  res.send("Version Logged successfully");
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -31,11 +48,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+    client: 'mysql2',
+    connection: {
+        host: '127.0.0.1',
+        database: 'world',
+        user: 'root',
+        password: 'Creeperman12!'
+    }
+}
